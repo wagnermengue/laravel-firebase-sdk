8fff451301e224f7da4ddbf05fe6c47de4a2ff66
Wagner Mengue <dev.mengue@gmail.com>
2018-02-14 23:46:59 -0200

    Adicionado forma de integração com SDK Admin Firebase

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
 Route::get('/', function () {
+
+
+    $serviceAccount = ServiceAccount::fromJsonFile('../'.env('GOOGLE_APPLICATION_CREDENTIALS'));
+
+    $firebase = (new Factory)
+        ->withServiceAccount($serviceAccount)
+        ->withDatabaseUri(env('FIREBASE_CREDENTIALS'))
+        ->create();
+
+    $database = $firebase->getDatabase();
+
+    $newPost = $database
+        ->getReference('blog/posts')
+        ->push([
+            'title' => 'Post title',
+            'body' => 'This should probably be longer.'
+        ]);
+
+    $database->getReference('config/website')
+        ->set([
+            'name' => 'My Application',
+            'emails' => [
+                'support' => 'support@domain.tld',
+                'sales' => 'sales@domain.tld',
+            ],
+            'website' => 'https://app.domain.tld',
+        ]);
+
+    $database->getReference('config/website/name')->set('New name');
+
+//    $newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+//    $newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+//
+//    $newPost->getChild('title')->set('Changed post title');
+//    $newPost->getValue(); // Fetches the data from the realtime database
+//    $newPost->remove();
+
     return view('welcome');
 });
